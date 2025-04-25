739b10ab018c361da6e91c8edbda48308b95e165
Sebastian Esch <sebastian.esch@ibsolution.de>
2025-04-25 09:52:42 +0000

    add handler implemenations and messages

--- srv/projects-service.js
+++ srv/projects-service.js
@@ -9,22 +9,43 @@ class ProjectsService extends cds.ApplicationService {
 
         this.on('isStarted', async function onIsStarted(request) {
             log.info('on isStarted - request.data', request.data, 'request.params:', request.params)
+            const project = await SELECT.one.from(Projects).where({ID: request.data.project })
+            const result = Date.parse(project.startDate) < new Date()
+            return request.reply(result)
         })
 
         this.on('endProject', async function onEndProject(request) {
             log.info('on endProject - request.data', request.data, 'request.params:', request.params)
+            const updateCount = await UPDATE(Projects).set({endDate: request.timestamp}).where({ID: request.data.project})
+            if (updateCount != 1) {
+                return request.error('UNABLE_TO_END_PROJECT', [request.data.project])
+            }
         })
 
         this.on('stop', 'Projects', async function onStopProjects(request) {
             log.info('on stop Projects - request.data', request.data, 'request.params:', request.params)
+            const projectId = request.params[0].ID
+            const endDate = request.data.endDate ? request.data.endDate : request.timestamp
+            const result = await UPDATE(Projects).set({endDate: endDate }).where({ID: projectId})
+            if (result != 1) {
+                return request.error('UNABLE_TO_END_PROJECT', [request.data.project])
+            }
         })
 
         this.on('hasStarted', 'Projects', async function onHasStartedProjects(request) {
             log.info('on hasStarted Projects - request.data', request.data, 'request.params:', request.params)
+            const projectId = request.params[0].ID
+            const project = await SELECT.one.from(Projects).where({ID: projectId })
+            const result = Date.parse(project.startDate) < request.timestamp
+            return request.reply(result)
         })
 
         this.on('hasEnded', 'Projects', async function onHasEndedProjects(request) {
             log.info('on hasEnded Projects - request.data', request.data, 'request.params:', request.params)
+            const projectId = request.params[0].ID
+            const project = await SELECT.one.from(Projects).where({ID: projectId })
+            const result = project.endDate != undefined && Date.parse(project.endDate) < request.timestamp
+            return request.reply(result)
         })
 
         // Default Handlers for Create, Read, Update and Delete have before, after and on handlers
